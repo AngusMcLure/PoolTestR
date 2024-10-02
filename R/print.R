d4d3c6f226d6340337ec1355dc7ff7923c5125bb
Caitlin Cherryh <24237077+caitlinch@users.noreply.github.com>
2024-10-02 16:27:19 +1000

    Complete draft of print method
    Used tibble formatting to nicely output comparable values.

--- R/print.R
+++ R/print.R
@@ -1,6 +1,6 @@
 #' Print method for HierPoolPrevOutput objects
 #' 
-#' @param object An object of class "HierPoolPrevOutput" returned by HierPoolPrev().
+#' @param object An object of class "HierPoolPrevOutput" as returned by HierPoolPrev().
 #'
 #' @return A \code{data.frame} with columns as output by HierPoolPrev, reformatted to be easily human-readable 
 #'
@@ -22,26 +22,21 @@
 print.HierPoolPrevOutput <- function(object) {
   # This function 
   icc_names <- attr(object$ICC, "dimnames")[[2]]
-  icc_inds <- 1:length(icc_names)
   trimmed_object <- object %>% 
     select(grep("ICC", names(object), value = T, invert = T))
-  icc_dfs <- lapply(icc_inds, extract_matrix_column_ICC, icc_names, object)
-  formatted_object <-  cbind(trimmed_object, icc_dfs) %>%
-    mutate(
-      across(where(~ is.numeric(.x)) && min(.x) >= 0.01, ~ digits(.x, 4)),
-      across(where(~ is.numeric(.x) && min(.x) < 0.01), ~ signif(.x, digits = 3))
-    )
-  print(formatted_object) # After formatting, use print method for data.frame
+  icc_tbls <- lapply(1:length(icc_names), extract_matrix_column_ICC, icc_names, object)
+  formatted_output <- bind_cols(trimmed_object, icc_tbls)
+  return(formatted_output)
 }
 
-extract_matrix_column_ICC <- function(i, icc_names, object){
+extract_matrix_column_ICC <- function(i, matrix_col_names, object){
   # This function returns the ICC columns for a single variable in one neat, human-readable data.frame
   # Input: an integer, the names of the matrix columns, and the HierPoolPrevOutput
-  if (i <= length(icc_names)){
-    icc_cols <- object %>% 
-      select(grep("ICC", fonames(object), value = T))
-    i_cols <- as.data.frame(lapply(names(icc_cols), function(x){icc_cols[[x]][ , i]}))
-    names(i_cols) <- paste0(icc_names[i], ".", names(icc_cols))
+  if (i <= length(matrix_col_names)){
+    matrix_cols <- object %>% 
+      select(grep("ICC", names(object), value = T))
+    i_cols <- as_tibble(lapply(names(matrix_cols), function(x){matrix_cols[[x]][ , i]}), .name_repair = "minimal")
+    names(i_cols) <- paste0(matrix_col_names[i], ".", names(matrix_cols))
     return(i_cols)
   } else {
     return(NULL)
