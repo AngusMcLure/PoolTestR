bebfb93a9cd8e374597f74817d5360484bbb1e03
Caitlin Cherryh <24237077+caitlinch@users.noreply.github.com>
2024-10-02 18:12:39 +1000

    Documenting the class constructor function

--- R/new_HierPoolPrevOutput.R
+++ R/new_HierPoolPrevOutput.R
@@ -1,6 +1,10 @@
-## Functions for new class: HierPoolPrevOutput
-
-# Constructor function
+#' Constructor for HierPoolPrevOutput class
+#' Allows for nicely-formatted human-readable output using a custom \code{print} method 
+#' Internal function
+#' 
+#' @param x a tibble output by the \code{\link{HierPoolPrev}} function
+#' 
+#' @noRd
 new_HierPoolPrevOutput <- function(x = tbl()) {
   stopifnot(is.tbl(x))
   
