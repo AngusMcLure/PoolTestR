801f484f85cbf5b0b0c1b91651bc6fd04a7d4e13
Caitlin Cherryh <24237077+caitlinch@users.noreply.github.com>
2024-10-08 14:39:37 +1100

    Condence HierPoolPrev code
    Move all code for HierPoolPrev into the HierPoolPrev file, including the class HierPoolPrevOutput and methods for that class.

--- R/HierPoolPrev.R
+++ R/HierPoolPrev.R
@@ -235,3 +235,69 @@ HierPoolPrev <- function(data,result,poolSize,hierarchy,...,
   out <- new_HierPoolPrevOutput(out)
   out
 }
+
+
+#' Constructor for HierPoolPrevOutput class
+#' Allows for nicely-formatted human-readable output using a custom \code{print} method 
+#' Internal function (don't export - users do not need to construct HierPoolPrevOutput objects)
+#' 
+#' @param x a tibble output by the \code{\link{HierPoolPrev}} function
+#' 
+#' @noRd
+new_HierPoolPrevOutput <- function(x = tbl()) {
+  stopifnot(is.tbl(x))
+  
+  prev_class <- class(x)
+  structure(x,
+            class = c("HierPoolPrevOutput", prev_class)
+  )
+}
+
+
+#' Print method for HierPoolPrevOutput objects
+#' S3 method
+#' 
+#' @param object An object of class "HierPoolPrevOutput" as returned by HierPoolPrev().
+#'
+#' @return A \code{data.frame} output by HierPoolPrev, in a human readable format
+#'
+#' @seealso \code{\link{HierPoolPrev}}
+#' 
+#' @method print HierPoolPrevOutput
+#' @export print.HierPoolPrevOutput
+#' 
+#' @noRd
+print.HierPoolPrevOutput <- function(x, ...) {
+  # This function reformats HierPoolPrevOutput into a human-readable tibble
+  icc_names <- attr(x$ICC, "dimnames")[[2]]
+  trimmed_object <- x %>% 
+    select(grep("ICC", names(x), value = T, invert = T))
+  icc_tbls <- lapply(icc_names, extract_matrix_column_ICC, x)
+  formatted_output <- as_tibble(bind_cols(trimmed_object, icc_tbls))
+  print(formatted_output)
+  return(invisible(formatted_output))
+}
+
+
+#' Extract the correlation columns for a single clustering variable
+#' Internal function
+#' 
+#' @param cluster_var a single clustering variable (i.e., the name of the one matrix-column)
+#' @param x an object of class "HierPoolPrevOutput"
+#' 
+#' @noRd
+extract_matrix_column_ICC <- function(cluster_var, x){
+  all_cluster_vars <- attr(x$ICC, "dimnames")[[2]]
+  if (cluster_var %in% all_cluster_vars){
+    matrix_cols <- x %>% 
+      select(grep("ICC", names(x), value = T))
+    i_cols <- as_tibble(lapply(names(matrix_cols), 
+                               function(x){matrix_cols[[x]][ , which(all_cluster_vars == cluster_var)]}), 
+                        .name_repair = "minimal")
+    names(i_cols) <- paste0(cluster_var, ".", names(matrix_cols))
+    return(i_cols)
+  } else {
+    return(NULL)
+  }
+}
+
