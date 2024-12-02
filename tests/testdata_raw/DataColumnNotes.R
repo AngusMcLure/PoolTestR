a845ba364fd436b61dae17f6bd0e63210f57b3f1
Caitlin Cherryh <24237077+caitlinch@users.noreply.github.com>
2024-12-02 18:19:29 +1100

    Delete data file - generated df inside each test instead

--- tests/testdata_raw/DataColumnNotes.R
+++
@@ -1,8 +0,0 @@
-DataColumnNotes <- rbind(SimpleExampleData %>% mutate_all(as.character),
-                         c("This is an example of a note in a column", "", "", "", "", ""),
-                         c("", "This is an example of a note in a column", "", "", "", ""),
-                         c("", "", "This is an example of a note in a column", "", "", ""),
-                         c("", "", "", "This is an example of a note in a column", "", ""),
-                         c("", "", "", "", "", "This is an example of a note in a column"),
-                         c("", "", "", "", "", ""),
-                         c(NA, NA, NA, NA, NA, NA))
