9097be799ee2524a4ed492c4009204ac962aee56
Caitlin Cherryh <24237077+caitlinch@users.noreply.github.com>
2024-12-09 17:52:04 +1100

    Add utility function to return incorrectly nested variables
    For a given inner and outer cluster name (where the outer cluster is larger in the hierarchy), the function `check_nesting_levels()` looks at each variable in the inner cluster column and checks how many unique outer clusters contain that value. If the value is included in multiple output clusters, the different outer cluster values for the given inner cluster value are recorded and output.

--- R/utils.R
+++ R/utils.R
@@ -6,6 +6,7 @@
 #'
 #' @keywords internal
 #' @noRd
+#' 
 extract_matrix_column_ICC <- function(cluster_var, x){
   all_cluster_vars <- attr(x$ICC, "dimnames")[[2]]
   if (cluster_var %in% all_cluster_vars){
@@ -25,6 +26,7 @@ extract_matrix_column_ICC <- function(cluster_var, x){
     return(NULL)
   }
 }
+
 
 #' Take a column and its confidence/credibility intervals and format into
 #'  pretty, human-readable format
@@ -67,8 +69,11 @@ pretty_format_ICC_column <- function(var_df){
 #' Custom round for ICC columns - maintain 4 sig figs across different
 #' magnitudes of values
 #'
+#' @param x a vector of numeric values
+#'
 #' @keywords internal
 #' @noRd
+#' 
 custom_round <- function(x) {
   min_x <- min(x)
   # Get absolute value of greatest magnitude in column
@@ -83,3 +88,76 @@ custom_round <- function(x) {
   }
   return(x_formatted)
 }
+
+
+#' Check nesting for hierarchy/cluster variables within a data frame
+#' 
+#' Given a dataframe and two columns from the hierarchy/
+#' sampling scheme, this function checks whether any variables in the inner
+#' level are present for multiple outer levels.
+#' 
+#' @param data A \code{data.frame} with one row for each pooled sampled and
+#'   columns for the size of the pool (i.e., the number of specimens / isolates /
+#'   insects pooled to make that particular pool), the result of the test of the
+#'   pool. It may also contain additional columns with additional information
+#'   (e.g. location where pool was taken) which can optionally be used for
+#'   stratifying the data into smaller groups and calculating prevalence by
+#'   group (e.g. calculating prevalence for each location).
+#' @param outer_cluster The outer (i.e., larger) cluster. Must be a column name 
+#' from \code{data}.
+#' @param inner_cluster The inner (i.e., smaller) cluster. Nested within the 
+#' \code{outer_cluster}. Must be a column name from \code{data}.
+#' 
+#' Assume we have Houses 1 and 2 in Village A and Houses 1 and 2 in Village B. 
+#' This function will return the values 1 and 2 for the Houses level, as there 
+#' are identical houses in both Villages.
+#' 
+#' To correct this, each location (i.e., in this example each House) should have
+#' a unique identifier. The houses could have Village names appended (e.g., A-1,
+#' A-2, B-1, B-2) or each house could be numbered sequentially (e.g., 1, 2, 3, 
+#' 4).
+#' 
+#' In the example above the \code{outer_cluster} is "Village" and the 
+#' \code{inner_cluster} is "House".
+#' 
+#' As in \code{PrepareClusterData()} and \code{CheckClusterVars()}, the 
+#' clustering/hierarchical columns are input into the function from largest to 
+#' smallest.
+#'
+#' @keywords internal
+#' @noRd
+#' 
+check_nesting_levels <- function(data, outer_cluster, inner_cluster) {
+  # Keep only unique rows for the two columns in the inner/outer cluster 
+  unique_df <- unique(data[, c(outer_cluster, inner_cluster)])
+  inner_vals <- unique(unique_df[[inner_cluster]])
+  # Check that the values of the inner_cluster column are present only in one
+  # outer_cluster 
+  inner_list <- vector(mode="list", length = length(inner_vals))
+  for (i in 1:length(inner_list) ){
+    i_inner <- inner_vals[[i]]
+    i_outer <- unique_df[which(unique_df[[inner_cluster]] == i_inner), outer_cluster]
+    inner_list[[i]] <- list("inner_vals" = i_inner,
+                            "outer_vals" = i_outer,
+                            "inner_cluster" = inner_cluster,
+                            "outer_cluster" = outer_cluster)
+  }
+  length_outer_vals <- 
+    unlist(
+      lapply(
+        1:length(inner_list),
+        function(x){
+          length(inner_list[[i]]$outer_vals)
+        }
+      )
+    )
+  repeated_inners <- which(length_outer_vals > 1)
+  if (length(repeated_inners) > 0){
+    repeated_inner_list <- inner_list[repeated_inners]
+    op <- as.data.frame(do.call(rbind, repeated_inner_list))
+  } else {
+    op <- NULL
+  }
+  return(op)
+}  
+
